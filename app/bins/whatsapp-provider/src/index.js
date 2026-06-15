16b7912302efe3d406b627ce714adcf4699cc43c
Elwizzy12 <chiahans123@gmail.com>
2026-06-15 15:05:01 +0100

    chore : changee  whatsapp provider version

--- app/bins/whatsapp-provider/src/index.js
+++ app/bins/whatsapp-provider/src/index.js
@@ -12,10 +12,6 @@ const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: process.env.DATA_PATH || path.join(__dirname, '../.wwebjs_auth')
     }),
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
-    },
     puppeteer: {
         args: [
             '--no-sandbox',
